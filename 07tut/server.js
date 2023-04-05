09fab675f97f6209e12818457c33f7d9d518188e
gabriel <gabrielfernandes254@gmail.com>
2023-04-05 20:46:40 -0300

    middleware

--- 07tut/server.js
+++ 07tut/server.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+
+const errorHandler = require("./middleware/errorHandler");
+const { logger } = require("./middleware/logEvents");
 
 const PORT = process.env.PORT || 3500;
+
+//custom middleware logger
+app.use(logger);
+
+//Crioss Origin resource sharing
+// list of sites allowed to acesses my api
+const whitelist = [
+  "https://www.yoursite.com",
+  "https://localhost:3500",
+  "https://www.google.com",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatu: 200,
+};
+
+app.use(cors(corsOptions));
 
 // bult-in middleware to handle urlencoded data
 // in other words, form data
@@ -40,5 +67,8 @@ app.get(
 app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
+
+//handle error
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
